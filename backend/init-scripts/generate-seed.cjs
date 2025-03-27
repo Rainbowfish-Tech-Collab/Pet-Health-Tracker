e8152ed364eb5454f62db8ff4e45bc13cf63ea45
Char B <charburton18@gmail.com>
2025-03-27 10:39:40 -0700

    serialized user id

--- backend/init-scripts/generate-seed.cjs
+++ backend/init-scripts/generate-seed.cjs
@@ -3,11 +3,10 @@ const path = require('path');
 const { faker } = require('@faker-js/faker');
 
 const generateUsers = (count) => {
-  let sql = 'INSERT INTO "user" (id, email, username, password_hashed, date_created, date_updated, profile_picture) VALUES\n';
+  let sql = 'INSERT INTO "user" (email, username, password_hashed, date_created, date_updated, profile_picture) VALUES\n';
   const values = [];
 
   for (let i = 0; i < count; i++) {
-    const id = i;
     const email = faker.internet.email();
     const username = faker.internet.username();
     const passwordHashed = faker.internet.password();
@@ -15,7 +14,7 @@ const generateUsers = (count) => {
     const dateUpdated = '2025-03-24 14:30:00';
     const profilePicture = faker.image.url();
   
-    values.push(`('${id}', '${email}', '${username}','${passwordHashed}', '${dateCreated}', '${dateUpdated}', '${profilePicture}')`)
+    values.push(`('${email}', '${username}','${passwordHashed}', '${dateCreated}', '${dateUpdated}', '${profilePicture}')`)
   }
 
   sql+= values.join(',\n') + ';';
--- backend/init-scripts/seed.sql
+++ backend/init-scripts/seed.sql
@@ -1,7 +1,7 @@
-INSERT INTO "user" (id, email, username, password_hashed, date_created, date_updated, profile_picture) VALUES
-('0', 'Mark.Huels-Rowe46@hotmail.com', 'Adriana28','Fmon__vmH_ZvQrW', '2025-03-24 14:30:00', '2025-03-24 14:30:00', 'https://picsum.photos/seed/6XXWxcpr5/1279/2673'),
-('1', 'Tatum_Johnston94@yahoo.com', 'Ruthie.Durgan5','NKcTho6pjzq0Ldi', '2025-03-24 14:30:00', '2025-03-24 14:30:00', 'https://loremflickr.com/107/709?lock=8786464489115495'),
-('2', 'Angelo32@yahoo.com', 'Osbaldo.Parisian20','C2JvW6u9o_2_Ywc', '2025-03-24 14:30:00', '2025-03-24 14:30:00', 'https://picsum.photos/seed/Nuouu/321/2081'),
-('3', 'Cedrick.Hammes75@hotmail.com', 'Leann41','KDkOVY4wbiET732', '2025-03-24 14:30:00', '2025-03-24 14:30:00', 'https://picsum.photos/seed/b6oplL/2420/2470'),
-('4', 'Nicolette51@gmail.com', 'Monserrate_Block64','N_hXB3rws_Etokl', '2025-03-24 14:30:00', '2025-03-24 14:30:00', 'https://loremflickr.com/1460/1705?lock=8510422639532702'),
-('5', 'Elinor51@yahoo.com', 'Pietro31','UNgQC4yYEh_BFTH', '2025-03-24 14:30:00', '2025-03-24 14:30:00', 'https://picsum.photos/seed/cZK9i53M/1737/2494');
+INSERT INTO "user" (email, username, password_hashed, date_created, date_updated, profile_picture) VALUES
+('Beth74@yahoo.com', 'Antone.Gleason29','tvUehuj17xtbrGK', '2025-03-24 14:30:00', '2025-03-24 14:30:00', 'https://loremflickr.com/3884/240?lock=568551754802080'),
+('Missouri.Keebler7@hotmail.com', 'Matilde47','qE9odtApGugWdmV', '2025-03-24 14:30:00', '2025-03-24 14:30:00', 'https://picsum.photos/seed/pO6paX1Xo/2859/3226'),
+('Jacey.Simonis31@hotmail.com', 'Caden_Kautzer','wMOBZF3ErimH7BA', '2025-03-24 14:30:00', '2025-03-24 14:30:00', 'https://loremflickr.com/1863/652?lock=3244574930304685'),
+('Mariana_Lakin95@gmail.com', 'Karl_Oberbrunner60','QXPL6tBR1QCUfI7', '2025-03-24 14:30:00', '2025-03-24 14:30:00', 'https://picsum.photos/seed/RO6Nja/159/2626'),
+('Justen_Kirlin@hotmail.com', 'Kobe_Gutmann77','691S0fFexYB8PfR', '2025-03-24 14:30:00', '2025-03-24 14:30:00', 'https://picsum.photos/seed/Aokzm7Gc/3532/1279'),
+('Marietta.Koss@hotmail.com', 'Kaylah43','gMet_CwhJh85Vpn', '2025-03-24 14:30:00', '2025-03-24 14:30:00', 'https://picsum.photos/seed/EBXhC2zeM/2611/2484');
