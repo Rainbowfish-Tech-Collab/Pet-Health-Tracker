9865e767db7b14874465d59394084dfb3cc22cae
Char B <charburton18@gmail.com>
2025-04-02 21:15:25 -0700

    pets seeded in db

--- backend/init-scripts/generate-seed.cjs
+++ backend/init-scripts/generate-seed.cjs
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const { faker } = require('@faker-js/faker');
 
+// Generates SQL INSERT statements for the "user" table
 const generateUsers = (count) => {
   let sql = 'INSERT INTO "user" (email, username, password_hashed, date_created, date_updated, profile_picture) VALUES\n';
   const values = [];
@@ -10,7 +11,7 @@ const generateUsers = (count) => {
     const email = faker.internet.email();
     const username = faker.internet.username();
     const passwordHashed = faker.internet.password();
-    const dateCreated = '2025-03-24 14:30:00';
+    const dateCreated = '2025-03-24 14:30:00'; 
     const dateUpdated = '2025-03-24 14:30:00';
     const profilePicture = faker.image.url();
   
@@ -22,10 +23,37 @@ const generateUsers = (count) => {
   return sql;
 };
 
+// Generates SQL INSERT statements for the pet table
+const generatePets = (count) => {
+  let sql = 'INSERT INTO "pet" (pet_breed_id, sex_id, "name", birthday, "description", date_created, date_updated, profile_picture) VALUES\n';
+  const values = [];
+
+  for (let i = 0; i < count; i++) {
+    const petBreedId = Math.ceil(Math.random()*130);
+    const sexId = Math.ceil(Math.random()*4);
+    const name = faker.animal.petName();
+    const birthday = '2020-03-24';
+    const description = 'This is my pet.';
+    const dateCreated = '2025-03-24 14:30:00';
+    const dateUpdated = '2025-03-24 14:30:00';
+    const profilePicture = faker.image.url();
+  
+    values.push(`('${petBreedId}', '${sexId}', '${name}', '${birthday}','${description}', '${dateCreated}', '${dateUpdated}', '${profilePicture}')`)
+  }
+
+  sql+= values.join(',\n') + ';';
+
+  return sql;
+};
+
 // Define the seed.sql file path inside /backend/init-scripts/
 const seedFilePath = path.join(__dirname, 'seed.sql');
 
 // Write the seed data to seed.sql
-fs.writeFileSync(seedFilePath, generateUsers(6), 'utf8');
+fs.writeFileSync(
+  seedFilePath,
+  [generateUsers(6), generatePets(6)].join('\n'),
+  'utf8'
+);
 
 console.log(`seed.sql generated successfully at ${seedFilePath}`);
--- backend/init-scripts/seed.sql
+++ backend/init-scripts/seed.sql
@@ -1,7 +1,14 @@
 INSERT INTO "user" (email, username, password_hashed, date_created, date_updated, profile_picture) VALUES
-('Beth74@yahoo.com', 'Antone.Gleason29','tvUehuj17xtbrGK', '2025-03-24 14:30:00', '2025-03-24 14:30:00', 'https://loremflickr.com/3884/240?lock=568551754802080'),
-('Missouri.Keebler7@hotmail.com', 'Matilde47','qE9odtApGugWdmV', '2025-03-24 14:30:00', '2025-03-24 14:30:00', 'https://picsum.photos/seed/pO6paX1Xo/2859/3226'),
-('Jacey.Simonis31@hotmail.com', 'Caden_Kautzer','wMOBZF3ErimH7BA', '2025-03-24 14:30:00', '2025-03-24 14:30:00', 'https://loremflickr.com/1863/652?lock=3244574930304685'),
-('Mariana_Lakin95@gmail.com', 'Karl_Oberbrunner60','QXPL6tBR1QCUfI7', '2025-03-24 14:30:00', '2025-03-24 14:30:00', 'https://picsum.photos/seed/RO6Nja/159/2626'),
-('Justen_Kirlin@hotmail.com', 'Kobe_Gutmann77','691S0fFexYB8PfR', '2025-03-24 14:30:00', '2025-03-24 14:30:00', 'https://picsum.photos/seed/Aokzm7Gc/3532/1279'),
-('Marietta.Koss@hotmail.com', 'Kaylah43','gMet_CwhJh85Vpn', '2025-03-24 14:30:00', '2025-03-24 14:30:00', 'https://picsum.photos/seed/EBXhC2zeM/2611/2484');
+('Cayla_Tromp56@yahoo.com', 'Jimmy.Balistreri','QFoAqTf4pYWYuD5', '2025-03-24 14:30:00', '2025-03-24 14:30:00', 'https://picsum.photos/seed/l8MPEY8k/630/3041'),
+('Andrew_Morar@hotmail.com', 'Glen9','pbrQevLZFLDzvLA', '2025-03-24 14:30:00', '2025-03-24 14:30:00', 'https://loremflickr.com/119/825?lock=2979067776893975'),
+('Vida_Heathcote@hotmail.com', 'Judd_Lang','wj1JHUnYAdG0ysy', '2025-03-24 14:30:00', '2025-03-24 14:30:00', 'https://picsum.photos/seed/OuMqB/507/2637'),
+('Ephraim75@gmail.com', 'Hayden_Keeling97','7eNpyei2L7w8gqy', '2025-03-24 14:30:00', '2025-03-24 14:30:00', 'https://picsum.photos/seed/qoG3eA/1532/1457'),
+('Travis.Kirlin10@yahoo.com', 'Ari_Legros','Jlw0cY5pTFm7wHH', '2025-03-24 14:30:00', '2025-03-24 14:30:00', 'https://loremflickr.com/2705/110?lock=2827915569926824'),
+('Rosalind_McDermott21@gmail.com', 'Maryam.Grady21','8QlTpMCjDbRk8zg', '2025-03-24 14:30:00', '2025-03-24 14:30:00', 'https://picsum.photos/seed/CgSa9/1699/3264');
+INSERT INTO "pet" (pet_breed_id, sex_id, "name", birthday, "description", date_created, date_updated, profile_picture) VALUES
+('30', '2', 'Hank', '2020-03-24','This is my pet.', '2025-03-24 14:30:00', '2025-03-24 14:30:00', 'https://picsum.photos/seed/GkGQvA/1160/1519'),
+('115', '4', 'Stella', '2020-03-24','This is my pet.', '2025-03-24 14:30:00', '2025-03-24 14:30:00', 'https://loremflickr.com/1494/111?lock=2947049162242431'),
+('79', '2', 'Louie', '2020-03-24','This is my pet.', '2025-03-24 14:30:00', '2025-03-24 14:30:00', 'https://loremflickr.com/2270/154?lock=5508574405664696'),
+('50', '4', 'Hank', '2020-03-24','This is my pet.', '2025-03-24 14:30:00', '2025-03-24 14:30:00', 'https://picsum.photos/seed/7DiE53/223/3476'),
+('16', '4', 'Tuffy', '2020-03-24','This is my pet.', '2025-03-24 14:30:00', '2025-03-24 14:30:00', 'https://loremflickr.com/1482/1587?lock=4949419541148868'),
+('129', '4', 'Lucy', '2020-03-24','This is my pet.', '2025-03-24 14:30:00', '2025-03-24 14:30:00', 'https://loremflickr.com/2659/2595?lock=334698399920986');
