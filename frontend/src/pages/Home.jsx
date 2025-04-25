80cd9fec0ffac86b8c68dfcefa958c6a42b9088c
Robin Allen <114621789+robinallenaz@users.noreply.github.com>
2025-04-24 23:22:56 -0500

    Added route to Home.jsx from App.jsx

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,4 @@
+//default is line graph
+//routes: GET /activities/activityId, GET /symptoms/symptomId, GET /istatId
+//Pet data log will show most recent entries, last 7 days. This will be displayed in a Pet Data Log table.
+//Navbar at the bottom will have three buttons, left button to access pet data log. Middle button is to add specific data entry. Right button will be to access settings.
