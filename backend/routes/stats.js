c7e5fac62a05df12e4b07019ac90db06809a65cb
Jacky Yang <jackyyang1670@gmail.com>
2025-04-30 01:07:33 -0700

    Updated stats route, now properly gets all the stats

--- backend/routes/stats.js
+++ backend/routes/stats.js
@@ -1,20 +1,41 @@
-import express from 'express';
-import pool from '../config/database.js';
+import express from "express";
+import pool from "../config/database.js";
 const router = express.Router();
 
 // GET all stat data entries for a specific pet at /:petId/all
-router.get('/:petId/all', async(req, res, next) => {
-  try{
-    const {petId} = req.params;  
-    const result = await pool.query('SELECT pet_id, description, date_updated,stat_date FROM stat WHERE pet_id = ($1)', [petId]);
-    if(!result) return res.status(404).json({error: 'Pet not found'});
-    res.json(result.rows);
-  }
-  catch(err){
-    console.error(err);
-    next(err);
-  }
-})
-
+router.get("/:petId/all", async (req, res, next) => {
+	try {
+		const { petId } = req.params;
+		const result = await pool.query(
+			`
+      SELECT 
+        stat.pet_id,
+        stat.description,
+        stat.date_updated,
+        stat.stat_date,
+        CASE
+          WHEN weight_stat.id IS NOT NULL THEN 'Weight'
+          WHEN glucose_stat.id IS NOT NULL THEN 'Glucose'
+          WHEN heart_rate_stat.id IS NOT NULL THEN 'Heart Rate'
+          WHEN respiratory_rate_stat.id IS NOT NULL THEN 'Respiratory'
+          WHEN other_stat.id IS NOT NULL THEN 'Other'
+          ELSE ''
+        END AS category
+      FROM stat
+        LEFT JOIN weight_stat ON stat.id = weight_stat.stat_id
+        LEFT JOIN glucose_stat ON stat.id = glucose_stat.stat_id
+        LEFT JOIN heart_rate_stat ON stat.id = heart_rate_stat.stat_id
+        LEFT JOIN respiratory_rate_stat ON stat.id = respiratory_rate_stat.stat_id
+        LEFT JOIN other_stat ON stat.id = other_stat.stat_id
+      WHERE stat.pet_id = $1`,
+			[petId]
+		);
+		if (!result) return res.status(404).json({ error: "Pet not found" });
+		res.json(result.rows);
+	} catch (err) {
+		console.error(err);
+		next(err);
+	}
+});
 
 export default router;
