4179a348e73b950d9e79656ccfa67353b0fda22f
Jacky Yang <jackyyang1670@gmail.com>
2025-05-02 10:23:19 -0700

    Updated minor changes

--- backend/routes/stats.js
+++ backend/routes/stats.js
@@ -9,6 +9,7 @@ router.get("/:petId/all", async (req, res, next) => {
 		const result = await pool.query(
 			`
       SELECT 
+        stat.id,
         stat.pet_id,
         stat.description,
         stat.date_updated,
@@ -19,7 +20,7 @@ router.get("/:petId/all", async (req, res, next) => {
           WHEN heart_rate_stat.id IS NOT NULL THEN 'Heart Rate'
           WHEN respiratory_rate_stat.id IS NOT NULL THEN 'Respiratory'
           WHEN other_stat.id IS NOT NULL THEN 'Other'
-          ELSE ''
+          ELSE 'Stat'
         END AS category
       FROM stat
         LEFT JOIN weight_stat ON stat.id = weight_stat.stat_id
@@ -27,7 +28,8 @@ router.get("/:petId/all", async (req, res, next) => {
         LEFT JOIN heart_rate_stat ON stat.id = heart_rate_stat.stat_id
         LEFT JOIN respiratory_rate_stat ON stat.id = respiratory_rate_stat.stat_id
         LEFT JOIN other_stat ON stat.id = other_stat.stat_id
-      WHERE stat.pet_id = $1`,
+      WHERE stat.pet_id = $1 
+      ORDER BY stat.date_updated DESC`,
 			[petId]
 		);
 		if (!result) return res.status(404).json({ error: "Pet not found" });
