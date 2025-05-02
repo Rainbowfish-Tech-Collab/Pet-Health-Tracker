413d3c01b23eb1f38c852445d30ae51791121760
Ken Yeh <yehken@gmail.com>
2025-05-02 10:12:30 -0700

    cleanup

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ const PORT = process.env.PORT || 3000;
 
 // CORS configuration
 app.use(cors({
-  origin: 'http://localhost:5173', // Your React app's URL
+  origin: 'http://localhost:5173',
   credentials: true
 }));
 
