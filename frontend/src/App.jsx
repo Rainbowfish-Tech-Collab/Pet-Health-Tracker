5922f367f0631bb584900ba033bdffa167753af8
Ken Yeh <yehken@gmail.com>
2025-06-12 21:41:03 -0700

    test Tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import SpecificDataEntry from './pages/SpecificDataEntry';
+import Login from './components/Login';
+import Register from './components/Register';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/" element={<Home />} />
         <Route path="/pet-data-log" element={<Home />} /> {/* For now, reuse Home component */}
         <Route path="/add-entry" element={<SpecificDataEntry />} />
